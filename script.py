85b9e082373c849d840e779e299db42abbbd1dee
Gaulthier Gain <gaulthier.gain@student.ulg.ac.be>
2018-02-05 20:20:44 +0100

    Parse IP network and compute broadcast address

--- script.py
+++ script.py
@@ -4,10 +4,11 @@ TODO: add team membres here
 
 Libs used:
 https://docs.python.org/2/library/re.html
-
+https://docs.python.org/3/library/ipaddress.html
 """
 
 import re
+import ipaddress
 
 filenameRange = 'range.txt'
 filenamePwd = 'password.txt'
@@ -19,8 +20,15 @@ try:
         ip_add = ''.join(file.readline().splitlines())
         if (ip_add == ''):
             break
-        print '[' + ip_add + ']'
-        #test
+
+        # Use ipaddress module to check the validity of ip address
+        ip_network = ipaddress.ip_network(unicode(ip_add, "UTF-8"), strict = False)
+        broadcast_address = ip_network.broadcast_address
+        
+        print ip_network
+        print broadcast_address
+except ValueError:
+    print 'Invalid IP address format'
 except IOError:
     print 'The file' + filenameRange + ' couldn\'t be found'
     exit()
